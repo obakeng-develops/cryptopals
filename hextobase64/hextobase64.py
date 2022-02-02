35fc6772bbca9ca333c80ce834be7ab6a29ae58a
Obakeng <mosadiobakeng7@gmail.com>
2022-02-02 09:45:17 +0200

    Fixed encode method

--- hextobase64/hextobase64.py
+++ hextobase64/hextobase64.py
@@ -4,5 +4,5 @@ hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6
 
 byte_array = bytearray.fromhex(hex)
 print(byte_array)
-base64_value = base64.encode(byte_array)
+base64_value = base64.b64encode(byte_array)
 print(base64_value)
