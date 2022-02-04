5ae7a85ec9fe15f18a0a45d493b72394ba79e076
Obakeng <mosadiobakeng7@gmail.com>
2022-02-04 14:53:52 +0200

    Updated python implementation with error handling

--- hextobase64/hextobase64.py
+++ hextobase64/hextobase64.py
@@ -1,8 +1,17 @@
+# Always operate on raw bytes, never on encoded strings.
+# CryptoPals
+# Obakeng Mosadi, 2022
+
 import base64
 
 hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
 byte_array = bytearray.fromhex(hex)
-print(byte_array)
-base64_value = base64.b64encode(byte_array)
+base64_value = None
+
+if byte_array:
+    base64_value = base64.b64encode(byte_array)
+else:
+    print("Could not convert to base 64.")
+
 print(base64_value)
