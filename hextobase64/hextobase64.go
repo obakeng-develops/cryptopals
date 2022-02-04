47e8287412a565e94a74dfe38cfc2be7b5ad0eda
Obakeng <mosadiobakeng7@gmail.com>
2022-02-04 14:15:18 +0200

    Finished hex to base64 implementation in go.

--- hextobase64/hextobase64.go
+++ hextobase64/hextobase64.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 )
 
 func main() {
 
-	hex := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	s := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-	base64_value := base64.StdEncoding.EncodeToString([]byte(hex))
-	fmt.Println(base64_value)
+	decodedByteArray, err := hex.DecodeString(s)
+
+	b64_value := base64.StdEncoding.EncodeToString(decodedByteArray)
+
+	if err != nil {
+		fmt.Println("Unable to convert hex to byte. ", err)
+	}
+
+	fmt.Printf("%s\n", decodedByteArray)
+	fmt.Println(b64_value)
 }
