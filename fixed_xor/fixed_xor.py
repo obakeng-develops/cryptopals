0907fb7f00f16fb7974754afa02dd4d855c0d852
Obakeng <mosadiobakeng7@gmail.com>
2022-02-08 10:42:49 +0200

    Updated fixed xor implementation in Python:

--- fixed_xor/fixed_xor.py
+++ fixed_xor/fixed_xor.py
@@ -6,17 +6,23 @@ from itertools import cycle
 
 def fixed_xor(stringOne, stringTwo):
 
-    stringOneByteArray = bytearray.fromhex(stringOne)
-    stringTwoByteArray = bytearray.fromhex(stringTwo)
+    binaryOne = bin(int(stringOne, 16))[2:]
+    binaryTwo = bin(int(stringTwo, 16))[2:]
 
-    base64StringOne = base64.b64encode(stringOneByteArray)
-    base64StringTwo = base64.b64encode(stringTwoByteArray)
+    checkLength = len(binaryOne) if len(
+        binaryOne) > len(binaryTwo) else len(binaryTwo)
 
-    if len(stringOneByteArray) != len(stringTwoByteArray):
-        print("Buffers are not of equal-length")
+    binaryOne = binaryOne.zfill(checkLength)
+    binaryTwo = binaryTwo.zfill(checkLength)
 
-    xored = bytes(a ^ b
-                  for (a, b) in zip(base64StringOne, cycle(base64StringTwo)))
+    xor_combination = [int(a) ^ int(b) for (a, b) in zip(binaryOne, binaryTwo)]
+
+    stringResult = "".join([str(bits) for bits in xor_combination])
+
+    final = hex(int(stringResult, 2))[2:]
+
+    print(final)
 
 
-fixed_xor("1c0111001f01010006", "1a024b53535009181c")
+fixed_xor("1c0111001f010100061a024b53535009181c",
+          "686974207468652062756c6c277320657965")
